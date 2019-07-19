7ff4a2f225b537ec4499bf7fd28927f75592eb1c
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-07-18 20:42:11 -0400

    Profiles do Spring

--- src/main/java/com/cesarbassani/algamoneyapi/AlgamoneyApiApplication.java
+++ src/main/java/com/cesarbassani/algamoneyapi/AlgamoneyApiApplication.java
@@ -1,9 +1,12 @@
 package com.cesarbassani.algamoneyapi;
 
+import com.cesarbassani.algamoneyapi.config.property.AlgamoneyApiProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cesarbassani/algamoneyapi/cors/CorsFilter.java
+++ src/main/java/com/cesarbassani/algamoneyapi/cors/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.cesarbassani.algamoneyapi.cors;
 
+import com.cesarbassani.algamoneyapi.config.property.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -13,17 +15,18 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    private String originPermitida = "http://localhost:8000"; //TODO: Configurar para diferentes ambientes
+    @Autowired
+    private AlgamoneyApiProperty algamoneyApiProperty;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.setHeader("Access-Control-Allow-Origin", originPermitida);
+        response.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getOriginPermitida());
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
-        if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+        if ("OPTIONS".equals(request.getMethod()) && algamoneyApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
             response.setHeader("Access-Control-Max-Age", "3600");
